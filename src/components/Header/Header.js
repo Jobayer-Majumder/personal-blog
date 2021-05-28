25037d19f222bf52fccc38ad945a665af44d636b
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-05-28 18:15:51 +0600

    header component initialize

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from '../../images/brand-log.png';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to="/">
+                        <img src={logo} className={styles.brandLogo} alt="brand-logo" />
+                    </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ms-auto">
+                            <a class="nav-link me-3 fw-bold" href="#">Home</a>
+                            <a class="nav-link me-3 fw-bold" href="#">Features</a>
+                            <a class="nav-link me-3 fw-bold" href="#">Pricing</a>
+                            <Link to='/login'>
+                                <button className="btn btn-outline-danger fw-bold">Login</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Header;
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header/Header';
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Header />
+        </>
     );
 };
 
